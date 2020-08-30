80709a4ed1e50ccf03196b087cbd29200b50b21d
Vanderson de Castro <vanderson.Castro@ufv.br>
2020-08-30 15:05:06 -0300

    criando pasta e movendo exercícios 30/08/2020

--- total_a_pagar.py
+++
@@ -1,9 +0,0 @@
-#entrada
-id1, qtd1, valor1 =map(float, input('Digite informações para peça 1: id, qtd, valor\n:').split())
-id2, qtd2, valor2 =map(float, input('Digite informações para peça 2: id, qtd, valor\n:').split())
-
-#processamento
-valor_total = (qtd1* valor1)+(qtd2*valor2)
-
-#saida
-print("Valor a pagar = R$ ",str("%.1"%valor_total))
