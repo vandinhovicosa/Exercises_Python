261ac691633373b202dfa7e1107917470d5dca4a
Vanderson de Castro <vanderson.Castro@ufv.br>
2020-08-30 15:10:33 -0300

    movendo exercícios 30/08/2020

---
+++ exercícios1/area_do_circulo.py
@@ -0,0 +1,10 @@
+#entrada
+print("Calculo da área de um circulo em Cm")
+c=float(input('Digite o 1 valor: '))
+
+
+#processamento
+area = 3.14159*c
+area = str("%.1f"%area)
+#saida
+print("A área do circulo  = ", area, "Cm")
