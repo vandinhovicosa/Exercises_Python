a033f8504787a1beb6405351fff66902279847ab
Vanderson de Castro <vanderson.Castro@ufv.br>
2020-09-24 10:45:47 -0300

    lista

---
+++ lista.py
@@ -0,0 +1,24 @@
+#lista
+
+lista = list(range(10))
+print(lista)
+
+lista.sort()
+print(lista)
+
+lista.append(12)
+print(lista)
+
+lista.pop()
+print(lista)
+
+lista.extend([12,14,15])
+print(lista)
+
+
+#lista + [12,14]
+#print(lista)
+
+registro =  ('renzo, ',35)
+nome, idade = registro
+print(nome, idade)
