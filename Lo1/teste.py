ee5148d4a16c516faa7cb949f5a91660a43f6a1f
Vanderson de Castro <vanderson.Castro@ufv.br>
2020-09-28 08:27:16 -0300

    exercicio types

---
+++ Lo1/teste.py
@@ -0,0 +1,9 @@
+"""
+Tipos
+"""
+print('Nome', type('Nome'))
+print('Vanderson', type('Vanderson'))
+print('Idade', type('Idade'))
+print(41, type(41))
+print('Sexo', type('Sexo'))
+print('True', type(True))
